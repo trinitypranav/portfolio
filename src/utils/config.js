78b36ddbc7293edfa2db082864313b686cb2009d
Pranav Jadhav <trinitypranav@gmail.com>
2023-12-26 20:37:52 +0530

    config js file

---
+++ src/utils/config.js
@@ -0,0 +1,279 @@
+export const workObjects = [
+  {
+    company: "Glued Tomatoes",
+    position: "Software Development Engineer - Backend",
+    startDate: "Feb 2022",
+    endDate: "Present",
+    location: "Remote",
+    bullets: [
+      "Accomplished 20% savings in engineering work hours by developing 6 Node.js scripts via Kinvey Flex to automate synchronization for 600+ customers app data, proactively averting potential issues",
+      "Addressed the need for improved logs management by revamping internal JavaScript utility library to capture execution details of backend scripts, achieving an outstanding 2x boost in issue investigation speed",
+      "Classified 600+ apps metadata (Customer, Internal, Sales, etc) using a Node.js script which will play an important role during data migrations in the future",
+    ],
+    techstack:
+      "JavaScript, Node.js, Kinvey, React, Jest, SQL, Kanban, Git, GitHub",
+  },
+  {
+    company: "Progress Software",
+    position: "Principal Engineer - Technical Support",
+    startDate: "Dec 2020",
+    endDate: "Feb 2022",
+    location: "Remote",
+    bullets: [
+      "Halved engineering-support friction within 3 months by gaining Elasticsearch-Kibana query expertise for operational intelligence and developed 2 dashboards for real-time monitoring of customer app health, log analytics and debugging critical issues proactively",
+      "Accelerated MongoDB operations by 5x for key clients by adeptly solving complex data issues through normalization solutions & high-impact indexes",
+    ],
+  },
+  {
+    company: "NewtGlobal Consulting",
+    position: "Senior Engineer - Technical Support",
+    startDate: "Sept 2018",
+    endDate: "Dec 2020",
+    location: "Remote",
+    bullets: [
+      "Gained exposure to Agile methodology through scrum and sprint activities with product and engineering leading to 30% improvement in bug fix rate",
+      "Enhanced backend + MongoDB skills & boosted proficiency across product suite by heading Kinvey Flex (Node.js microservices framework) support, achieving notable 70% reduction in full-stack query resolution time",
+    ],
+  },
+  {
+    company: "Mindscrub Technologies",
+    position: "Software Engineer",
+    startDate: "July 2013",
+    endDate: "Sept 2018",
+    location: "Pune, MH",
+    bullets: [
+      "Led frontend SDKs support for Kinvey BaaS, built demo apps for customers showcasing key features resulted in around 20% cut in support traffic and improved resolution times achieving customer delight",
+      "Initiated and directed data formatting project at XOLights, doubled the efficiency of 8-member team by automating workflows using Excel Macros",
+    ],
+  },
+];
+
+export const projectObjects = [
+  {
+    name: "Portfolio Website",
+    image: "https://i.ibb.co/VSWbvMr/portfolio-Website-Screenshot.png",
+    bullets: [
+      "Built highly scalable & responsive single page application end-to-end in 3 days using React & Tailwind which loads blazingly fast within 10ms due to efficient lazy loading implementation",
+      "Achieved quick navigations using client side routing and a minified performant production build using Parcel bundler",
+    ],
+    demoLink: "https://trinitypranav.github.io/learning-react/",
+    githubLink: "https://github.com/trinitypranav/learning-react",
+    techstack: "react, tailwind, react-router-dom, parcel, jest",
+  },
+  {
+    name: "Portfolio Website",
+    image: "",
+    bullets: [
+      "Production read highly scalable SPA which loads within 200ms end to end",
+      "responsive design using tailwind css",
+      "client side routing using react-router-dom",
+    ],
+    demoLink: "https://trinitypranav.github.io/learning-react/",
+    githubLink: "https://github.com/trinitypranav/learning-react",
+    techstack: "react, tailwind, react-router-dom, parcel, jest",
+  },
+];
+
+export const educationObjects = [
+  {
+    company: "Scaler Academy",
+    position: "Fullstack Development Track",
+    startDate: "Oct 2022",
+    endDate: "Feb 2024",
+    location: "Online",
+    bullets: [
+      "Solved 500+ problems on Data Structures and Algorithms. Thoroughly studied Time & Space Complexity, Arrays, Hashmap, Bit Manipulation, 2 Pointers Technique, Recursion, Sort and Search Techniques, Strings, Linked Lists, Stacks, Queues, Trees and BST, Heaps, Tries, Dynamic Programming, Greedy Algorithms, Backtracking and Graphs",
+      "Mastered SQL and Databases by writing 200+ queries related to CRUD, Joins, Aggregate and Window functions. Studied concepts of Indexing, Transactions and Schema Design in depth. Practiced by designing schemas of Scaler, Video Streaming apps, Food Delivery Apps, E-Commerce apps, Hospital and Insurance Apps, etc",
+      "Learnt Fullstack Development - HTML, CSS, JavaScript, React, Redux, Node.js, Express, MongoDB by developing 6 projects. Added Unit and Integration tests using React Testing Library and Jest. Deployed to GitHub Pages and Netlify using Parcel",
+      "Understood important concepts in System Design - Consistent Hashing, CAP Theorem, Master-Slave, SQL vs NoSQL, Orchestration & Shard Creation, Zookeeper + Kafka, Elastic Search, S3 + Quad Trees, Designed Uber and Hotstar, Microservices",
+      "Now studying Product Management for Software Engineers",
+    ],
+  },
+  {
+    company: "Bachelor of Engineering in Computer Science",
+    position: "Pune University",
+    startDate: "Aug 2009",
+    endDate: "Aug 2013",
+    location: "Offline",
+    bullets: ["Department Topper"],
+  },
+];
+
+export const skillsObjects = [
+  {
+    name: "Amazon Leadership Principles",
+    image: "https://i.ibb.co/xfvMN5L/amazon.png",
+    level: "familiar",
+  },
+  {
+    name: "Data Structures & Algorithms",
+    image: "https://i.ibb.co/YXtVvHJ/dsa.png",
+    level: "proficient",
+  },
+  {
+    name: "Product Management",
+    image: "https://i.ibb.co/znWGMdR/productmanagement.png",
+    level: "familiar",
+  },
+  {
+    name: "Object Oriented Programming",
+    image: "https://i.ibb.co/t4WGYGQ/oop.png",
+    level: "familiar",
+  },
+  {
+    name: "CSS",
+    image: "https://i.ibb.co/p41cGZB/css.png",
+    level: "proficient",
+  },
+  {
+    name: "Express",
+    image: "https://i.ibb.co/VY4CHgz/express.png",
+    level: "familiar",
+  },
+  {
+    name: "Firebase",
+    image: "https://i.ibb.co/F6bQvTZ/firebase.png",
+    level: "familiar",
+  },
+  {
+    name: "GIT",
+    image: "https://i.ibb.co/6yq5y9c/git.png",
+    level: "proficient",
+  },
+  {
+    name: "GitHub",
+    image: "https://i.ibb.co/KKLBkmq/github.png",
+    level: "proficient",
+  },
+  {
+    name: "HTML",
+    image: "https://i.ibb.co/jGt0d63/html.png",
+    level: "proficient",
+  },
+  {
+    name: "JavaScript",
+    image: "https://i.ibb.co/Ns48rxF/javascript.png",
+    level: "proficient",
+  },
+  {
+    name: "Jest",
+    image: "https://i.ibb.co/qrTdXTV/jest.png",
+    level: "familiar",
+  },
+  {
+    name: "React Testing Library",
+    image: "https://i.ibb.co/Nj7RJL8/reacttestinglibrary.png",
+    level: "familiar",
+  },
+  {
+    name: "Jira",
+    image: "https://i.ibb.co/kq6Bx5Y/jira.png",
+    level: "proficient",
+  },
+  {
+    name: "MongoDB",
+    image: "https://i.ibb.co/yFBbN84/mongodb.png",
+    level: "familiar",
+  },
+  {
+    name: "SQL",
+    image: "https://i.ibb.co/dg13mKj/mysql.png",
+    level: "proficient",
+  },
+  {
+    name: "Node.JS",
+    image: "https://i.ibb.co/BczhCR4/nodejs.png",
+    level: "proficient",
+  },
+  {
+    name: "OpenAI",
+    image: "https://i.ibb.co/YyQgb0t/openai.png",
+    level: "familiar",
+  },
+  {
+    name: "Progress Kinvey",
+    image: "https://i.ibb.co/W0nHGPz/progress-Kinvey.jpg",
+    level: "proficient",
+  },
+  {
+    name: "React",
+    image: "https://i.ibb.co/6H0jrNN/react.png",
+    level: "proficient",
+  },
+  {
+    name: "Redux Toolkit",
+    image: "https://i.ibb.co/5GfCQqj/redux.png",
+    level: "familiar",
+  },
+  {
+    name: "Tailwind CSS",
+    image: "https://i.ibb.co/m6m54WG/tailwindcss.jpg",
+    level: "proficient",
+  },
+  {
+    name: "Stripe",
+    image: "https://i.ibb.co/tKrDpz1/stripe.png",
+    level: "familiar",
+  },
+  {
+    name: "OpenAI",
+    image: "https://i.ibb.co/YyQgb0t/openai.png",
+    level: "familiar",
+  },
+  {
+    name: "Prompt Engineering",
+    image: "https://i.ibb.co/M1G7Xsg/prompt.png",
+    level: "familiar",
+  },
+  {
+    name: "System Design",
+    image: "https://i.ibb.co/gz142Df/systemdesign.png",
+    level: "proficient",
+  },
+  {
+    name: "REST",
+    image: "https://i.ibb.co/cXRwnmq/rest.png",
+    level: "proficient",
+  },
+  {
+    name: "Elasticsearch",
+    image: "https://i.ibb.co/g7CY77s/elasticsearch.png",
+    level: "familiar",
+  },
+  {
+    name: "Kibana",
+    image: "https://i.ibb.co/54VKjh7/kibana.png",
+    level: "proficient",
+  },
+  {
+    name: "Agile Methodology & Scrum",
+    image: "https://i.ibb.co/GCPYBry/agile.png",
+    level: "familiar",
+  },
+];
+
+export const certificateObjects = [
+  {
+    name: "Scaler Expert MOCK Interview - DSA",
+    image: "https://i.ibb.co/mGHt1Bv/MockDSA.jpg",
+    bullets: [
+      "Time & Space Complexity | Arrays - Prefix Sum, Carry Forward, Sub-arrays, Sliding Window | 2D Matrices | Hashmap | Bit Manipulation | 2 Pointers Technique | Recursion | Sorting and Searching (Binary Search) Techniques | Strings | Linked Lists | Stacks and Queues | Trees and BST - Inorder, Preorder, Postorder, Level order traversal | Heaps | Tries | Dynamic Programming | Greedy Algorithms | Backtracking | Graphs - BFS & DFS",
+    ],
+  },
+  {
+    name: "Scaler Expert MOCK Interview - SQL & Databases",
+    image:
+      "https://moonshot-assets.s3.us-west-2.amazonaws.com/generated/6d4bf5d6d456560f00437e80570dd00d964b4a403644c18be98b469ac7afa6c0?AWSAccessKeyId=ASIAY7MGLG6ZE564BBGC&Expires=1703231731&Signature=Xpps98paXJINsuK4onuD3coW7TI%3D&x-amz-security-token=IQoJb3JpZ2luX2VjEJ%2F%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaCXVzLXdlc3QtMiJHMEUCIE1RRtOIWQmktLvt86cIq31sgl5G1ffon6EQ%2FVbAiHvcAiEA4D6cL8CM2kf0%2FpgQh%2FvL5lVdXijOaYAj%2FNGuhr82o7Uq9wMIKBADGgw2MTcxNDY0MzE0MTAiDDFR%2BjdSLLOr9MrRUCrUA1M1IRlmnsGjSGbaRZZHC2EKHVwl7u%2Fe%2BCsHrpMK0AwZBX%2F6BknujPgs3giuidIAFIRvOULk7sFGcN67PTXDASALfKw3No31TDrgTiNtoR5LNXJKv0i8QeJ%2FGlzqIf6JlO%2B9yVnJn%2FiixN3Xebi49NLf49LwBIxP6G15OZlhcXBsem979Bo25TwDGP84WM6urx4EgyaKFMMxaSPbNOpKlsphbU0q5hH%2By3XbYKIfJPBz%2F%2FCJ7Uoj0XksX51zWCtgdG3u8%2BHWlR8Po2DIMYw%2Fv0OlKAVvVoMmDWM7MfjuqlIe73WmJXXSvWgRVmoln%2BPQRrxpQIqSFONxjfS3nMxewMy9jAMXYwT7i%2F3hfgZ5nEb88nVkcYENGAg4DfaX0GVCr8KvKUTAOjH1ZwuraU12zUwDAnU4sRbNhD9zPLU53MH8ADJjcJ4JjY5n%2F9gVcLfj1lMx78CRaBBSqSQRv56LvYoaK7yexR3AjNlkfUcBEXH%2FQePVhZAjlzCWyevs5jr1CiKNIJVH19B9xE22owhsZq82eqOT1Rllhy6yEOjJzsRSPCTUpwgTEIusFrwWxT14juUpeq9N65%2Be5pCs7FV5Jy%2BQBFn8Il7XWO%2FK%2B9sY6tK3vBgbdzCV2ZSsBjqlARfwToQ7R%2FAEff%2BgssTx9%2BleMMbVmESm70SItL%2FT5eqEFg1XgVpT0o%2FRTakBKJmlzClHQKODk2md1oxpX1shPriP1f3eqrU3U6eFf2uc205q28w7sRpXLIRj88WK4GDuDHI2oXEWVK6S8g%2FzAW3OqLO6cZVs9pxQ6b%2BmCnSyEruucJ9TBGdwfUVzo5sI5tp315Uw35%2BNmHMCnmmHR4I0ej%2Bbk20EYg%3D%3D",
+    bullets: [
+      "Relational databases and DBMS, SQL, Keys, CRUD operations, Where, Group By, Having, IN operator, NULL handling, Order By, Limit & Offset, LIKE operator, Joins, Aggregate Queries, Sub-queries, Union and Union All, Indexing, Transactions, ACID, Isolation levels, Shared Locks & Exclusive Locks, Schema Design, Normalisation and Normalisation Forms, Cardinalities, Primary & Foreign Keys, Views & Window Functions",
+    ],
+  },
+  {
+    name: "Scaler Expert MOCK Interview - JavaScript",
+    image:
+      "https://moonshot-assets.s3.us-west-2.amazonaws.com/generated/df2be98adde3c2d0b60f4d929b0bda34232ba78bef991e50a07e7d081c5dccf7?AWSAccessKeyId=ASIAY7MGLG6ZE564BBGC&Expires=1703233596&Signature=TWF1eJ4Fm4ZceJee02%2F0%2Fa%2F20Qo%3D&x-amz-security-token=IQoJb3JpZ2luX2VjEJ%2F%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaCXVzLXdlc3QtMiJHMEUCIE1RRtOIWQmktLvt86cIq31sgl5G1ffon6EQ%2FVbAiHvcAiEA4D6cL8CM2kf0%2FpgQh%2FvL5lVdXijOaYAj%2FNGuhr82o7Uq9wMIKBADGgw2MTcxNDY0MzE0MTAiDDFR%2BjdSLLOr9MrRUCrUA1M1IRlmnsGjSGbaRZZHC2EKHVwl7u%2Fe%2BCsHrpMK0AwZBX%2F6BknujPgs3giuidIAFIRvOULk7sFGcN67PTXDASALfKw3No31TDrgTiNtoR5LNXJKv0i8QeJ%2FGlzqIf6JlO%2B9yVnJn%2FiixN3Xebi49NLf49LwBIxP6G15OZlhcXBsem979Bo25TwDGP84WM6urx4EgyaKFMMxaSPbNOpKlsphbU0q5hH%2By3XbYKIfJPBz%2F%2FCJ7Uoj0XksX51zWCtgdG3u8%2BHWlR8Po2DIMYw%2Fv0OlKAVvVoMmDWM7MfjuqlIe73WmJXXSvWgRVmoln%2BPQRrxpQIqSFONxjfS3nMxewMy9jAMXYwT7i%2F3hfgZ5nEb88nVkcYENGAg4DfaX0GVCr8KvKUTAOjH1ZwuraU12zUwDAnU4sRbNhD9zPLU53MH8ADJjcJ4JjY5n%2F9gVcLfj1lMx78CRaBBSqSQRv56LvYoaK7yexR3AjNlkfUcBEXH%2FQePVhZAjlzCWyevs5jr1CiKNIJVH19B9xE22owhsZq82eqOT1Rllhy6yEOjJzsRSPCTUpwgTEIusFrwWxT14juUpeq9N65%2Be5pCs7FV5Jy%2BQBFn8Il7XWO%2FK%2B9sY6tK3vBgbdzCV2ZSsBjqlARfwToQ7R%2FAEff%2BgssTx9%2BleMMbVmESm70SItL%2FT5eqEFg1XgVpT0o%2FRTakBKJmlzClHQKODk2md1oxpX1shPriP1f3eqrU3U6eFf2uc205q28w7sRpXLIRj88WK4GDuDHI2oXEWVK6S8g%2FzAW3OqLO6cZVs9pxQ6b%2BmCnSyEruucJ9TBGdwfUVzo5sI5tp315Uw35%2BNmHMCnmmHR4I0ej%2Bbk20EYg%3D%3D",
+    bullets: [
+      "Scope | Closures | Var, Let & Const | Data Types | Garbage Collection | Code Execution Context, Event Loop & ES6 | Temporal Dead Zone | Hoisting | Currying | Higher Order Functions | DOM manipulations | Events and Events Handling | APIs | Local and Session Storage | Debouncing & Throttling | Async/ Defer | OOP - Constructor Functions, Classical & Prototypal Inheritance | Call, Apply, Bind | Polyfills | Asynchronous Programming - setTimeout, setInterval, Callbacks, Promises & Promise APIs, Async Await | this keyword | Shallow & Deep copy | Spread & Rest operator",
+    ],
+  },
+];
