e0ffa0dcda5b1e5a39461556debc8b882eae8d77
Pranav Jadhav <trinitypranav@gmail.com>
2024-01-14 19:52:02 +0530

    body for the home page of portfolio

---
+++ src/components/Body.js
@@ -0,0 +1,12 @@
+import React from "react";
+import BodySummaryCard from "./BodySummaryCard";
+
+const Body = () => {
+  return (
+    <div id="body" className="p-6">
+      <BodySummaryCard />
+    </div>
+  );
+};
+
+export default Body;
